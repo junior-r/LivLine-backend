4153e415d1ca1ca4c185f2f9ac34e6eb9919ee47
junior31064049@gmail.com <junior31064049@gmail.com>
2025-05-13 20:32:22 -0500

    add production origin to cors

--- src/middlewares/cors.ts
+++ src/middlewares/cors.ts
@@ -1,10 +1,6 @@
 import cors from 'cors'
 
-const ACCEPTED_ORIGINS = [
-  'http://localhost:5173',
-  'http://localhost:3000',
-  'https://api.avengers.com',
-]
+const ACCEPTED_ORIGINS = ['http://localhost:5173', 'https://liv-line-frontend.vercel.app/']
 
 export const corsMiddleware = ({ acceptedOrigins = ACCEPTED_ORIGINS } = {}) =>
   cors({
